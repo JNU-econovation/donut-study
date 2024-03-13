3fc2f607fc18ac99c6cf2301307c190ad36b3c7f
dxwwan <kkw1700@gmail.com>
2024-03-13 15:36:24 +0900

    feat(common): Header 공통 컴포넌트 추가

---
+++ src/common/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = ({ missionNumber }) => {
+  return (
+    <header>
+      <h1>Donut Study</h1>
+      <h6>Here is page for Mission-{missionNumber}</h6>
+    </header>
+  );
+};
