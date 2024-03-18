c1afc08782156a41e798ca9220b33d4f42146443
Klomachenko <leekyumin0901@naver.com>
2024-03-18 18:02:35 +0900

    chore: 절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@css", replacement: "/src/css" },
+    ]
+  }
 })
