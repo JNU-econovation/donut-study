fd7bc7d70ac71456b63a9c4f7477376302a1e973
Klomachenko <leekyumin0901@naver.com>
2024-03-18 17:32:51 +0900

    style: theme.js 설정
    Description: fontSize, backgroundColor, color, borderColor를 미리
    설정합니다

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export const fontSize = {
+  title: '1.5rem',
+  content: '0.75rem',
+};
+
+export const backgroundColor = {
+  black: '#000',
+  white: '#fff',
+  green: '#1EB649',
+};
+
+export const color = {
+  black: '#000',
+  white: '#fff',
+  green: '#1EB649',
+};
+
+export const borderColor = {
+  black: '#000',
+  white: '#fff',
+  green: '#1EB649',
+};
