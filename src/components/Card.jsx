ebb39255cd6d9090745edee41d2d285f176d68d2
Klomachenko <leekyumin0901@naver.com>
2024-03-19 02:55:14 +0900

    feat: Dark-Mode 구현
    Description: 다크모드를 구현하였습니다. Dark 버튼을 클릭시 실행됩니다

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,27 +2,40 @@ import '@css/Card.css';
 import ProfileImage from '@image/ProfileImage.png';
 import { fontSize, fontWeight } from '../styles/theme';
 import { CARD_CONTENTS } from '../constants/CardData';
+import { useState } from 'react';
 
 const Card = () => {
   const githubUrl = 'https://github.com/Klomachenko';
   const youtubeUrl = 'https://www.youtube.com/channel/UCpxKdR3scZUtwjz_E-uIDyw';
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle('dark-mode', darkMode);
+  };
 
   return (
-    <div className='card-layout'>
+    <div className={`card-layout ${darkMode ? 'dark-mode' : ''}`}>
       <div className='left-box'>
         <img src={ProfileImage} alt='Profile Image' />
       </div>
       <div className='right-box'>
         <div className='title-box'>
           <h1
-            style={{ fontSize: fontSize.title, fontWeight: fontWeight.heavy }}
+            style={{
+              fontSize: fontSize.title,
+              fontWeight: fontWeight.heavy,
+              color: darkMode ? 'white' : 'black',
+            }}
           >
             Kyumin Lee
           </h1>
         </div>
         <div className='content-box'>
           {CARD_CONTENTS.map((content, index) => (
-            <p key={index}>{content}</p>
+            <p key={index} style={{ color: darkMode ? 'white' : 'black' }}>
+              {content}
+            </p>
           ))}
         </div>
         <div className='button-box'>
@@ -39,6 +52,9 @@ const Card = () => {
             }}
           >
             YouTube
+          </button>
+          <button onClick={toggleDarkMode}>
+            {darkMode ? 'Light' : 'Dark'}
           </button>
         </div>
       </div>
