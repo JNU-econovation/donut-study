1986e321080e85a5fe1355e5c0d1294ba9f79356
Klomachenko <leekyumin0901@naver.com>
2024-03-18 17:49:59 +0900

    feat: create Card Component
    Description: 카드 컴포넌트를 생성합니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import Card from './components/Card';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div>
+      <p>카드입니다</p>
+    </div>
+  );
+};
+
+export default Card;
