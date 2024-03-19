c9f193124f73c13cdee3b33e0215ce7cd51c24ec
hai <xgkg0344@naver.com>
2024-03-19 17:04:38 +0900

    feat: 소개글 추가

--- src/components/CardIntro.jsx
+++ src/components/CardIntro.jsx
@@ -5,7 +5,9 @@ const CardIntro = () => {
   return (
     <div className="CardIntro">
       <img className="image" src={image1} />
+
       <div>
+        <h1>Econovation 26th 강하은</h1>
         안녕하세요 저는 이번에 같이 리액트 스터디 하게 된 강하은입니다.
         <br />
         스터디 열심히 참여하면서 많이 배우고 같이 성장해 나가고 싶습니다! <br />
