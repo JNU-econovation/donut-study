744c212dec0bbf431721a70ccd82e5bdfa1f7fd5
hai <xgkg0344@naver.com>
2024-03-19 16:34:46 +0900

    feat: 이미지 경로 설정 및 추가

--- src/components/CardIntro.jsx
+++ src/components/CardIntro.jsx
@@ -1,7 +1,10 @@
 import "./CardIntro.css";
+import image1 from "../assets/Image.jpg";
+import "./image.css";
 const CardIntro = () => {
   return (
     <div className="CardIntro">
+      <img className="image" src={image1} />
       <div>
         안녕하세요 저는 이번에 같이 리액트 스터디 하게 된 강하은입니다.
         <br />
