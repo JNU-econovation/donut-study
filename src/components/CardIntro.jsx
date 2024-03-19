87e3a0f88d3ae92b570df05ba2ec0fbb2e527e51
hai <xgkg0344@naver.com>
2024-03-19 16:09:49 +0900

    feat: CardIntro 소개글 추가

---
+++ src/components/CardIntro.jsx
@@ -0,0 +1,15 @@
+import "./CardIntro.css";
+const CardIntro = () => {
+  return (
+    <div className="CardIntro">
+      <div>
+        안녕하세요 저는 이번에 같이 리액트 스터디 하게 된 강하은입니다.
+        <br />
+        스터디 열심히 참여하면서 많이 배우고 같이 성장해 나가고 싶습니다! <br />
+        한 학기 잘 해보아요~
+      </div>
+    </div>
+  );
+};
+
+export default CardIntro;
