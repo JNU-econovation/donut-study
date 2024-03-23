d95315e82a20f80fe4298765511f229e7d655b85
Klomachenko <leekyumin0901@naver.com>
2024-03-23 19:22:12 +0900

    feat: CouponModal 구현

---
+++ src/components/block/CouponModal.jsx
@@ -0,0 +1,29 @@
+import styles from '@css/CouponModal.module.css';
+import DefaultButton from '../atom/DefaultButton';
+import Coupon from '../atom/Coupon';
+
+const CouponModal = () => {
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.buttonBox}>
+          <DefaultButton />
+        </div>
+        <div className={styles.contentBox}>
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+          <Coupon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CouponModal;
