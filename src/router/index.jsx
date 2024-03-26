3a8902d3d958860aa215d549f06b19a851cd5c14
dxwwan <kkw1700@gmail.com>
2024-03-26 12:55:03 +0900

    feat(mission2): 쿠폰 라우 추가

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { NameCardPage } from "../pages/NameCard.page";
+import { CouponEventPage } from "../pages/CouponEvent.page";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/mission1" element={<NameCardPage />} />
+      <Route path="/mission2" element={<CouponEventPage />} />
     </Routes>
   );
 }
