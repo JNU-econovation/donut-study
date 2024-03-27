ebcdd885996fd353b306960124d5d66d229bca18
dxwwan <kkw1700@gmail.com>
2024-03-27 14:49:05 +0900

    refactor(mission2): 쿠폰 훅 로직 변경

--- src/hooks/useCoupon.js
+++ src/hooks/useCoupon.js
@@ -1,23 +1,39 @@
 import { useState } from "react";
 
 export const useCoupon = () => {
-  const [coupon, setCoupon] = useState(0);
-  const isCouponFull = () => {
-    return coupon === 10;
-  };
+	const [coupon, setCoupon] = useState(0);
+	const isCouponFull = () => {
+		return coupon === 10;
+	};
 
-  const getCoupon = () => {
-    setCoupon(coupon + 1);
-  };
+	const isCouponOver = () => {
+		return coupon > 10;
+	};
 
-  const orderByCoupon = () => {
-    if (isCouponFull()) {
-      alert("쿠폰 10개를 모아 커피를 받으세요!");
-      setCoupon(0);
-      return;
-    }
-    alert("쿠폰이 부족합니다.");
-  };
+	const isCouponLack = () => {
+		return coupon < 10;
+	};
 
-  return { coupon, isCouponFull, getCoupon, orderByCoupon };
+	const getCoupon = () => {
+		setCoupon(coupon + 1);
+	};
+
+	const orderByCoupon = () => {
+		switch (true) {
+			case isCouponFull():
+				alert("쿠폰 10개를 모두 모으셨습니다. 커피를 드립니다.");
+				setCoupon(0);
+				break;
+			case isCouponOver():
+				alert("쿠폰 10개를 모두 모았습니다. 커피를 드립니다.");
+				setCoupon(coupon - 10);
+				break;
+			case isCouponLack():
+				alert("쿠폰이 부족합니다.");
+				break;
+			default:
+				break;
+		}
+	};
+	return { coupon, isCouponFull, getCoupon, orderByCoupon };
 };
