f0ffb1383ff48f42e51238c4b0b49a80447b3538
박건규 <geongyu09@gmail.com>
2024-03-29 21:29:33 +0900

    feat: 이미지 alt 값 추가

--- src/components/Stamp.jsx
+++ src/components/Stamp.jsx
@@ -4,7 +4,11 @@ import checkedStamp from "../../../../../../../src/assets/complete-stamp.png";
 export default function Stamp({ isChecked }) {
   return (
     <div className="inline-block p-10 border-l border-b">
-      <img src={isChecked ? stamp : checkedStamp} className="w-14 h-14" />
+      <img
+        src={isChecked ? stamp : checkedStamp}
+        className="w-14 h-14"
+        alt="order stamp"
+      />
     </div>
   );
 }
