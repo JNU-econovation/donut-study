f7db8412c12ca600864dd3384b7e6b31e1c8ec98
dxwwan <kkw1700@gmail.com>
2024-03-29 19:43:12 +0900

    feat(mission3): 신용카드 페이지 라우팅 추가

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { NameCardPage } from "../pages/NameCard.page";
 import { CouponEventPage } from "../pages/CouponEvent.page";
+import { CreditCardPage } from "../pages/CreditCard.page";
 
 export function Router() {
-  return (
-    <Routes>
-      <Route path="/mission1" element={<NameCardPage />} />
-      <Route path="/mission2" element={<CouponEventPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/mission1" element={<NameCardPage />} />
+			<Route path="/mission2" element={<CouponEventPage />} />
+			<Route path="/mission3" element={<CreditCardPage />} />
+		</Routes>
+	);
 }
