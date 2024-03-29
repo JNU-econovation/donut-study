a14838134fbeee55cc15f0f043d3954b08f37a80
박건규 <geongyu09@gmail.com>
2024-03-29 20:45:14 +0900

    refactor: Props 이름 통일
    같은 값을 받는 props의 이름을 통일합니다

--- src/components/StampPaper.jsx
+++ src/components/StampPaper.jsx
@@ -1,12 +1,12 @@
 import Stamp from "./Stamp";
 
-export default function StampPaper({ order }) {
+export default function StampPaper({ orderCount }) {
   return (
     <div className="text-start mx-auto border-r border-t w-[690px] overflow-hidden">
-      {Array.from({ length: order }).map((_, index) => (
+      {Array.from({ length: orderCount }).map((_, index) => (
         <Stamp key={index} isChecked={false} />
       ))}
-      {Array.from({ length: 10 - order }).map((_, index) => (
+      {Array.from({ length: 10 - orderCount }).map((_, index) => (
         <Stamp key={index} isChecked={true} />
       ))}
     </div>
