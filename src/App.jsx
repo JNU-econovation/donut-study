98e5a349431ca2ea020a94ac5908ee19efe30a6e
박건규 <geongyu09@gmail.com>
2024-03-29 21:25:10 +0900

    refactor: 불필요한 컴포넌트 분리 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import OrderBtn from "./components/OrderBtn";
 import SubmitBtn from "./components/SubmitBtn";
 import StampPaper from "./components/StampPaper";
-import StampHeader from "./components/StampHeader";
 import { MAX_ORDER_COUNT } from "./constants/order";
 
 export default function App() {
@@ -18,7 +17,11 @@ export default function App() {
 
   return (
     <div className="text-center">
-      <StampHeader />
+      <h1 className="text-3xl text-center font-bold mt-20">쿠폰함</h1>
+      <p className="mt-12 ">
+        쿠폰을 받아보세요! 주문할 때마다 쿠폰을 받을 수 있어요! 10개를 모으면
+        무려 1개가 공짜??
+      </p>
       <OrderBtn orderCount={order} increaseOrderCount={orderCoffee} />
       <StampPaper orderCount={order} />
       <SubmitBtn submitCupon={submitCupon} orderCount={order} />
