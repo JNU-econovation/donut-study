87347259cc741ab27d658d263ce9a65f157dff69
박건규 <geongyu09@gmail.com>
2024-03-29 21:18:50 +0900

    refactor: Stamp의 조건부 수정

--- src/components/Stamp.jsx
+++ src/components/Stamp.jsx
@@ -4,11 +4,7 @@ import checkedStamp from "/src/assets/complete-stamp.png";
 export default function Stamp({ isChecked }) {
   return (
     <div className="inline-block p-10 border-l border-b">
-      {isChecked ? (
-        <img src={stamp} className="w-14 h-14" />
-      ) : (
-        <img src={checkedStamp} className="w-14 h-14" />
-      )}
+      <img src={isChecked ? stamp : checkedStamp} className="w-14 h-14" />
     </div>
   );
 }
