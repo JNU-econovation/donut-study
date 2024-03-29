95f2058529c5ebe1948d558b5fa92e39d14935f2
박건규 <geongyu09@gmail.com>
2024-03-29 21:23:01 +0900

    feat: uuid v4를 사용하여 StampPaper의 map key값이 중복되지 않도록 수정

--- src/components/StampPaper.jsx
+++ src/components/StampPaper.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { MAX_ORDER_COUNT } from "../constants/order";
 import Stamp from "./Stamp";
 
@@ -5,7 +6,7 @@ export default function StampPaper({ orderCount }) {
   return (
     <div className="text-start mx-auto border-r border-t w-[690px] overflow-hidden">
       {Array.from({ length: MAX_ORDER_COUNT }).map((_, index) => (
-        <Stamp key={index} isChecked={index >= orderCount} />
+        <Stamp key={uuidv4()} isChecked={index >= orderCount} />
       ))}
     </div>
   );
