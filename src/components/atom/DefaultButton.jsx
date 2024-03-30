ec2246564a07a486675ff33f07e2b83ab0a3a8bd
Klomachenko <leekyumin0901@naver.com>
2024-03-31 03:51:11 +0900

    feat: DefaultButton 구현

---
+++ src/components/atom/DefaultButton.jsx
@@ -0,0 +1,7 @@
+import styles from '../../css/DefaultButton.module.css';
+
+const DefaultButton = () => {
+  return <button className={styles.button}>버튼</button>;
+};
+
+export default DefaultButton;
