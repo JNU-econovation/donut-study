a876c0b580f5651bac1ea95d8440db9fd4a6202d
hai <xgkg0344@naver.com>
2024-03-31 21:31:47 +0900

    style: Card css 변경

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
-import "./Card.css";
 import CardIntro from "./CardIntro";
+import styled from "styled-components";
 const Card = () => {
   return (
-    <div className="Card">
+    <CardFrame>
       <CardIntro />
-    </div>
+    </CardFrame>
   );
 };
 
 export default Card;
+
+const CardFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ebeaec;
+  box-shadow: 0px 2px 7px rgb(0, 0, 0, 0.3);
+  width: 46rem;
+  height: 13rem;
+  margin: 0 auto;
+  border-radius: 0.3125rem;
+  text-align: center;
+  padding: 1rem;
+`;
