26c2d9eed62148b2fa9f9fb9fe0b34969399d3df
Klomachenko <leekyumin0901@naver.com>
2024-03-31 21:44:38 +0900

    feat: CardNumberInput value 받아오기 구현

--- src/components/block/CardNumberInput.jsx
+++ src/components/block/CardNumberInput.jsx
@@ -1,18 +1,67 @@
+import { useEffect, useState } from 'react';
 import styles from '../../css/CardNumberInput.module.css';
 import DefaultInput from '../atom/DefaultInput';
 
 const CardNumberInput = () => {
+  const [eachNumber, setEachNumber] = useState({
+    firstNumb: '',
+    secondNumb: '',
+    thirdNumb: '',
+    fourthNumb: '',
+  });
+
+  const [cardNumber, setCardNumber] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEachNumber((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(e.target.value);
+  };
+
+  const combinedCardNumber = Object.values(eachNumber).join(' ');
+
+  useEffect(() => {
+    setCardNumber(combinedCardNumber);
+  }, [eachNumber]);
+
   return (
     <div className={styles.container}>
       <p>카드 번호</p>
       <div className={styles.inputContainer}>
-        <DefaultInput placeholder={'0 0 0 0'} />
+        <DefaultInput
+          placeholder={'0 0 0 0'}
+          onChange={handleChange}
+          value={setEachNumber.firstNumb}
+          type='text'
+          maxLength='4'
+        />
         <span>-</span>
-        <DefaultInput placeholder={'0 0 0 0'} />
+        <DefaultInput
+          placeholder={'0 0 0 0'}
+          onChange={handleChange}
+          value={setEachNumber.secondNumb}
+          type='text'
+          maxLength='4'
+        />
         <span>-</span>
-        <DefaultInput placeholder={'0 0 0 0'} />
+        <DefaultInput
+          placeholder={'0 0 0 0'}
+          onChange={handleChange}
+          value={setEachNumber.thirdNumb}
+          type='text'
+          maxLength='4'
+        />
         <span>-</span>
-        <DefaultInput placeholder={'0 0 0 0'} />
+        <DefaultInput
+          placeholder={'0 0 0 0'}
+          onChange={handleChange}
+          value={setEachNumber.fourthNumb}
+          type='text'
+          maxLength='4'
+        />
       </div>
     </div>
   );
