c17d09d8abf7236570cd5a3972be077d3ee7f648
dxwwan <kkw1700@gmail.com>
2024-04-01 19:37:50 +0900

    chore: strictMode 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
