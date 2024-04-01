4436bdb43731b1afc6b372d790f0de7ad16fc036
박건규 <geongyu09@gmail.com>
2024-04-01 11:32:00 +0900

    refactor: InputField 분자에서 전달하는 props 리팩토링

--- src/components/molecules/InputFIeld/InputField.jsx
+++ src/components/molecules/InputFIeld/InputField.jsx
@@ -1,25 +1,32 @@
 import styles from "./InputField.module.css";
 import Input from "../../atoms/Input/Input";
 import Label from "../../atoms/Label/Label";
+import { useId } from "react";
 
 export default function InputField({
   inputLabel,
   inputType,
   placeholder,
-  id,
   value,
   onChange,
 }) {
+  const id = useId();
+  const inputProps = {
+    inputLabel,
+    inputType,
+    placeholder,
+    id,
+    value,
+    onChange,
+  };
+  const inputLabelProps = {
+    inputLabel,
+    id,
+  };
   return (
     <div className={styles.InputField}>
-      <Label inputLabel={inputLabel} id={id} />
-      <Input
-        inputType={inputType}
-        id={id}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      <Label {...inputLabelProps} />
+      <Input {...inputProps} />
     </div>
   );
 }
