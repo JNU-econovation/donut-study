b3501774124fadf984291492017bc9a1a5596d85
mlnwns <mlnwns@naver.com>
2024-04-02 16:47:55 +0900

    feat: 회색 배경화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import CardRegisterForm from "./components/CardRegisterForm";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <GreyBackground>
+      <CardRegisterForm />
+    </GreyBackground>
+  );
 }
 
 export default App;
+
+const GreyBackground = styled.div`
+  background-color: #efeff0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
