646b4002dd20cd8900d72479f1508a60fb9a4e2c
hai <xgkg0344@naver.com>
2024-04-02 22:47:44 +0900

    feat: 카드 상태관리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useRef, useState } from "react";
+import CardList from "./components/CardList";
+import CardEditor from "./components/CardEditor";
+import styled from "styled-components";
 
 function App() {
-  return <div></div>;
+  const [cards, setCards] = useState([]);
+  const cardsRef = useRef(0);
+
+  const cardRegister = (name, number, expirationDate, cvc, password) => {
+    const newCard = {
+      id: cardsRef.current++,
+      name: name,
+      number: number,
+      expirationDate: expirationDate,
+      cvc: cvc,
+      password: password,
+    };
+    setCards([newCard, ...cards]);
+  };
+
+  return <></>;
 }
 
 export default App;
