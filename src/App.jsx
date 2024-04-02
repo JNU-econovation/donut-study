479c816c4806218a3c3ce6150f22cf9e63d4b54d
Klomachenko <leekyumin0901@naver.com>
2024-04-02 16:22:54 +0900

    feat: react-toastify 메세지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import MainPage from './pages/MainPage';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,6 +10,7 @@ import {
   dateCheck,
   passWordCheck,
 } from '../utils/validation';
+import { ToastContainer, toast } from 'react-toastify';
 
 const MainPage = () => {
   const [cardList, setCardList] = useState([]);
@@ -21,6 +22,10 @@ const MainPage = () => {
   const [cardNnumberValid, setCardNumberValid] = useState(false);
 
   const removeCard = useRemove(cardList, setCardList);
+
+  const notify = () => {
+    toast('');
+  };
 
   const handleButtonClick = (cardInfo) => {
     const isCardNameValid = cardNameCheck(cardInfo.cardName);
@@ -39,25 +44,33 @@ const MainPage = () => {
       const newCard = { ...cardInfo, id: nextId };
       setCardList((prevCardList) => [...prevCardList, newCard]);
       setNextId(nextId + 1);
+      toast.success('카드를 성공적으로 등록하셨습니다!');
     } else if (!isCardNameValid) {
       setCardNameValid(false);
+      toast.error('카드 이름을 올바르게 입력해주세요');
     } else if (!isCvcNumberValid) {
       setcvcNumberValid(false);
+      toast.error('CVC 번호를 올바르게 입력해주세요');
     } else if (!isDateValid) {
       setDateValid(false);
+      toast.error('유효 기간을 올바르게 입력해주세요!');
     } else if (!isPassWordValid) {
       setPasswordValid(false);
+      toast.error('비밀번호를 올바르게 입력해주세요!');
     } else if (!isCardNumberValid) {
       setCardNumberValid(false);
+      toast.error('카드번호를 올바르게 입력해주세요!');
     }
   };
 
   const handleRemoveButtonClick = (targetId) => {
     removeCard(targetId);
+    toast.success('카드를 삭제하셨습니다.');
   };
 
   return (
     <div className={styles.container}>
+      <ToastContainer />
       <MainLayout
         onButtonClick={handleButtonClick}
         cardNameValid={cardNameValid}
@@ -65,6 +78,7 @@ const MainPage = () => {
         dateValid={dateValid}
         passwordValid={passwordValid}
         cardNnumberValid={cardNnumberValid}
+        notify={notify}
       />
       {cardList.map((cardInfo, index) => (
         <CardListModal
