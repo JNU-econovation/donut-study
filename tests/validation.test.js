49536d81efc1fad09a70e751a543a42a03f049c8
Klomachenko <leekyumin0901@naver.com>
2024-04-02 11:53:15 +0900

    feat: expirationDate 테스트 코드 구현

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -1,4 +1,4 @@
-import { cardNameCheck, cvcNumberCheck } from '../src/utils/validation';
+import { cardNameCheck, cvcNumberCheck, dateCheck } from '../src/utils/validation';
 
 describe('cardNameCheck 함수', () => {
   test('공백이 아니며, 공백을 포함하지 않고, 특수 문자가 없는 경우', () => {
@@ -33,5 +33,31 @@ describe('cvcNumberCheck 함수', () => {
 
   test('빈 문자열인 경우', () => {
     expect(cvcNumberCheck('')).toBe(false);
+  });
+});
+
+describe('dateCheck 함수', () => {
+  test('공백이 아니며, 공백을 포함하지 않고, 숫자이며 4글자이고, 연, 월이 제대로 작성된 경우', () => {
+    expect(dateCheck('0725')).toBe(true);
+  });
+
+  test('4글자가 넘는 경우', () => {
+    expect(dateCheck('12345')).toBe(false);
+  });
+
+  test('특수 문자가 포함된 경우', () => {
+    expect(dateCheck('12!')).toBe(false);
+  });
+
+  test('빈 문자열인 경우', () => {
+    expect(dateCheck('')).toBe(false);
+  });
+
+  test('월이 잘못된 경우', () => {
+    expect(dateCheck('1342')).toBe(false);
+  });
+
+  test('년도가 잘못된 경우', () => {
+    expect(dateCheck('0723')).toBe(false);
   });
 });
