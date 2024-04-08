675db4caa2089ed0cde06e47693fcd909c549e5d
mlnwns <mlnwns@naver.com>
2024-04-08 19:52:32 +0900

    feat: 상품 목록 토글버튼 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-function App() {
-  return <div></div>;
-}
+import { useEffect } from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [showSaleProduct, setShowSaleProduct] = useState(false);
+
+  function handleShowSale() {
+    setShowSaleProduct(!showSaleProduct);
+  }
+  const fetchProducts = useEffect(() => {
+    const url = showSaleProduct
+      ? "api/sale_products.json"
+      : "api/sale_products.json";
+    fetchProducts(url);
+  }, []);
+  return (
+    <div>
+      <button onClick={handleShowSale}>
+        {showSaleProduct ? "All Product" : "Sale Product"}
+      </button>
+    </div>
+  );
+};
 
 export default App;
+
+<button>All products</button>;
