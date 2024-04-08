c573874074696c944fed8b707267396596a6f6c9
Klomachenko <leekyumin0901@naver.com>
2024-04-09 02:22:27 +0900

    feat: 상품 state 위치 변경

--- src/page/ShopPage.jsx
+++ src/page/ShopPage.jsx
@@ -1,10 +1,37 @@
-import Item from '../components/molecule/Item';
+import React, { useState, useEffect } from 'react';
+import DefaultButton from '../components/atom/DefaultButton';
+import ItemList from '../components/organism/ItemList';
 import styles from '../css/ShopPage.module.css';
 
 function ShopPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const itemData = async () => {
+      try {
+        const response = await fetch('/api/products.json');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('상품을 받아오지 못했습니다');
+      }
+    };
+
+    itemData();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className={styles.container}>
-      <Item />
+      <div className={styles.buttonBox}>
+        <DefaultButton buttonText='All' />
+        <DefaultButton buttonText='on Sale' />
+        <DefaultButton buttonText='price low-high' />
+      </div>
+      <div className={styles.itemBox}>
+        <ItemList products={products} />
+      </div>
     </div>
   );
 }
