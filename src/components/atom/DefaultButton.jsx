73f33d0ba9083e7404cf846c13f16d7676c225bd
Klomachenko <leekyumin0901@naver.com>
2024-04-09 03:00:39 +0900

    feat: DefaultButton props 수정

--- src/components/atom/DefaultButton.jsx
+++ src/components/atom/DefaultButton.jsx
@@ -1,7 +1,11 @@
 import styles from '../../css/DefaultButton.module.css';
 
-function DefaultButton({ buttonText }) {
-  return <button className={styles.button}>{buttonText}</button>;
+function DefaultButton({ buttonText, onClick }) {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {buttonText}
+    </button>
+  );
 }
 
 export default DefaultButton;
