6d06231d1217fe73abe1d6bef1b993df77722a55
mlnwns <mlnwns@naver.com>
2024-04-09 09:18:31 +0900

    style: 상품 페이지CSS 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [showSaleProduct, setShowSaleProduct] = useState(false);
+  const [products, setProducts] = useState([]);
 
   function handleShowSale() {
     setShowSaleProduct(!showSaleProduct);
   }
+
   function fetchProducts(url) {
     axios
-      .get(url) // 요청을 보낼 URL
+      .get(url)
       .then((response) => {
         console.log(response.data);
+        setProducts(response.data);
       })
       .catch((error) => {
         console.error("Error :", error);
@@ -25,15 +28,23 @@ const App = () => {
       : "api/sale_products.json";
     fetchProducts(url);
   }, [showSaleProduct]);
+
   return (
     <div>
       <button onClick={handleShowSale}>
-        {showSaleProduct ? "All Product" : "Sale Product"}
+        {showSaleProduct ? "Sale Product" : "All Product"}
       </button>
+      <div className="product-container">
+        {products.map((product) => (
+          <div key={product.id} className="product-item">
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <img src={product.src} alt={product.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default App;
-
-<button>All products</button>;
