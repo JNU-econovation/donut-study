eed2360d3d76e3f7a245d11a6dfd137c997a0029
hai <xgkg0344@naver.com>
2024-04-09 17:49:58 +0900

    feat: 오류 처리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ function App() {
       )
       .then((res) => {
         setProducts(res.data);
+      })
+      .catch((error) => {
+        console.error("상품 데이터를 불러오는 중 오류가 발생했습니다.", error);
       });
   }, [currentView]);
   return (
