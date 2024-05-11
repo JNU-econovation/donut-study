540056c2dd3f9bc4a6dfab9e0551994e8368a930
박건규 <geongyu09@gmail.com>
2024-05-11 12:54:30 +0900

    feat: tailwind 환경 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <section>
+      <h1 className="text-lg">그룹 스터디룸 6인 (최소 3인)</h1>
+      <nav className="bg-slate-950">
+        <ul>
+          {daysOfWeek.map((item, index) => (
+            <li key={`${item}-${index}`}>{item}</li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
 }
 
-export default App
+export default App;
