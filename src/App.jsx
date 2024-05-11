dd7f770e402c4172175182a5c0fc4e94c9fc5146
박건규 <geongyu09@gmail.com>
2024-05-11 18:43:34 +0900

    feat: 예약 타임 테이블 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
+import { useEffect, useState } from "react";
+import TimeTable from "./components/TimeTable";
+
 const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
+const timeline = [
+  { id: 1, time: "9:00" },
+  { id: 2, time: "9:30" },
+  { id: 3, time: "10:00" },
+  { id: 4, time: "10:30" },
+  { id: 5, time: "11:00" },
+  { id: 6, time: "11:30" },
+  { id: 7, time: "12:00" },
+  { id: 8, time: "12:30" },
+  { id: 9, time: "13:00" },
+  { id: 10, time: "13:30" },
+  { id: 11, time: "14:00" },
+  { id: 12, time: "14:30" },
+  { id: 13, time: "15:00" },
+  { id: 14, time: "15:30" },
+  { id: 15, time: "16:00" },
+  { id: 16, time: "16:30" },
+  { id: 17, time: "17:00" },
+  { id: 18, time: "17:30" },
+  { id: 19, time: "18:00" },
+  { id: 20, time: "18:30" },
+  { id: 21, time: "19:00" },
+  { id: 22, time: "19:30" },
+];
+
 function App() {
+  const [timeData, setTimeData] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    try {
+      fetch("api/time", { signal })
+        .then((res) => res.json())
+        .then((data) => setTimeData(data));
+    } catch (error) {
+      console.error(error);
+    }
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <section>
-      <h1 className="text-lg">그룹 스터디룸 6인 (최소 3인)</h1>
-      <nav>
-        <ul className="flex">
-          {daysOfWeek.map((item, index) => (
-            <li key={`${item}-${index}`}>{item}</li>
+      <header className="text-lg">그룹 스터디룸 6인 (최소 3인)</header>
+      <nav className="flex gap-8">
+        <ul>
+          <h2 className="text-center">요일</h2>
+          {timeline.map(({ id, time }) => (
+            <li
+              key={id}
+              className="border-b-2 border-solid border-gray-400 p-2 text-center"
+            >
+              {time}
+            </li>
+          ))}
+        </ul>
+        <ul className="flex gap-8">
+          {daysOfWeek.map((day, index) => (
+            <TimeTable key={`${day}-${index}`} timeData={timeData} day={day} />
           ))}
         </ul>
       </nav>
---
+++ src/components/TimeTable.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import ModalCreator from "./modal/ModalCreator";
+
+const defaultTimes = [
+  { id: 0, time: "9:00", isSet: false, isHalf: false, Deadline: "9:30" },
+  { id: 1, time: "9:30", isSet: false, isHalf: true, Deadline: "10:00" },
+  { id: 2, time: "10:00", isSet: false, isHalf: false, Deadline: "10:30" },
+  { id: 3, time: "10:30", isSet: false, isHalf: true, Deadline: "11:00" },
+  { id: 4, time: "11:00", isSet: false, isHalf: false, Deadline: "11:30" },
+  { id: 5, time: "11:30", isSet: false, isHalf: true, Deadline: "12:00" },
+  { id: 6, time: "12:00", isSet: false, isHalf: false, Deadline: "12:30" },
+  { id: 7, time: "12:30", isSet: false, isHalf: true, Deadline: "13:00" },
+  { id: 8, time: "13:00", isSet: false, isHalf: false, Deadline: "13:30" },
+  { id: 9, time: "13:30", isSet: false, isHalf: true, Deadline: "14:00" },
+  { id: 10, time: "14:00", isSet: false, isHalf: false, Deadline: "14:30" },
+  { id: 11, time: "14:30", isSet: false, isHalf: true, Deadline: "15:00" },
+  { id: 12, time: "15:00", isSet: false, isHalf: false, Deadline: "15:30" },
+  { id: 13, time: "15:30", isSet: false, isHalf: true, Deadline: "16:00" },
+  { id: 14, time: "16:00", isSet: false, isHalf: false, Deadline: "16:30" },
+  { id: 15, time: "16:30", isSet: false, isHalf: true, Deadline: "17:00" },
+  { id: 16, time: "17:00", isSet: false, isHalf: false, Deadline: "17:30" },
+  { id: 17, time: "17:30", isSet: false, isHalf: true, Deadline: "18:00" },
+  { id: 18, time: "18:00", isSet: false, isHalf: false, Deadline: "18:30" },
+  { id: 19, time: "18:30", isSet: false, isHalf: true, Deadline: "19:00" },
+  { id: 20, time: "19:00", isSet: false, isHalf: false, Deadline: "19:30" },
+  { id: 21, time: "19:30", isSet: false, isHalf: true, Deadline: "20:00" },
+];
+// let selectedTime = null;
+
+// eslint-disable-next-line react/prop-types
+export default function TimeTable({ day }) {
+  const [modalOn, setModalOn] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(null);
+  return (
+    <div>
+      <h2 className="text-center">{day}</h2>
+      <ul>
+        {defaultTimes.map(({ id, time }) => (
+          <li key={`${id}-${day}-${time}`} className="truncate">
+            <button
+              type="button"
+              className="border-b-2 p-2 text-green-700"
+              onClick={() => {
+                setSelectedTime(time);
+                setModalOn(true);
+              }}
+            >
+              예약 가능
+            </button>
+          </li>
+        ))}
+      </ul>
+      {modalOn && <ModalCreator close={() => setModalOn(false)} />}
+    </div>
+  );
+}
