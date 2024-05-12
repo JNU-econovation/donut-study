e08158184e54c6307b17989c97ac1aa376490085
박건규 <geongyu09@gmail.com>
2024-05-12 17:35:03 +0900

    feat: Modal 스타일 변경

--- src/components/modal/ModalCreator.jsx
+++ src/components/modal/ModalCreator.jsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
-import ReservationModal from "./Modal";
+import ReservationModal from "./ReservationModal";
 
 // eslint-disable-next-line react/prop-types
 export default function ModalCreator({ close }) {
