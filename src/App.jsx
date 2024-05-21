36fb29d731b4c566f75bbde08cc82a6777b1e370
mlnwns <mlnwns@naver.com>
2024-05-21 09:48:34 +0900

    feat: styled-component 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>도넛 블로그</div>;
+};
+
+export default Header;
