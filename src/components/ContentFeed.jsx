a641528d1dbd78b93af187bf61f63d20fd53fb63
박건규 <geongyu09@gmail.com>
2024-06-02 18:32:57 +0900

    feat: content 영역의 스타일 추가
    content 영역의 style를 추가합니다.
    - 기본 image 를 추가합니다.
    - 컴포넌트에 css 를 추가합니다.

--- src/components/ContentFeed.jsx
+++ src/components/ContentFeed.jsx
@@ -1,15 +1,48 @@
-// eslint-disable-next-line react/prop-types
+import styled from "styled-components";
+import { useImageUrlQuery } from "../hooks/querys/useContentQuery";
+import defaultImage from "../assets/default.png";
+
 export default function ContentFeed({ contentItem }) {
-  const { title, content, tags } = contentItem;
+  const { title, content, id } = contentItem;
+  const { data, error } = useImageUrlQuery(id);
+
+  const publicUrl = data ? `${data?.publicUrl}.png` : defaultImage;
+
   return (
-    <li>
-      <h2>{title}</h2>
-      <p>{content}</p>
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    </li>
+    <StyledContentFeed>
+      {error && <p>Error: {error.message}</p>}
+      <div
+        className="content-image"
+        style={{
+          backgroundImage: `url(${publicUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      <div className="content-detail">
+        <h2>{title}</h2>
+        <p>{content}</p>
+      </div>
+    </StyledContentFeed>
   );
 }
+
+const StyledContentFeed = styled.li`
+  height: 20rem;
+  border: 1px solid #ccc;
+  .content-image {
+    width: 100%;
+    height: 200px;
+    border-bottom: 1px solid #ccc;
+  }
+  .content-detail {
+    border: 1px solid #ccc;
+    padding: 1rem;
+  }
+  h2 {
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 0.5rem;
+  }
+`;
--- src/components/ContentGrid.jsx
+++ src/components/ContentGrid.jsx
@@ -1,16 +1,24 @@
+import styled from "styled-components";
 import ContentFeed from "./ContentFeed";
-import useContentQuery from "./querys/useContentQuery";
+import { useContentsQuery } from "../hooks/querys/useContentQuery";
 
 export default function ContentGrid() {
-  const { data, isLoading, error } = useContentQuery();
+  const { data, isLoading, error } = useContentsQuery();
 
   return (
-    <ul>
+    <StyledContentGrid>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {data?.map((contentItem) => (
         <ContentFeed key={contentItem.id} contentItem={contentItem} />
       ))}
-    </ul>
+    </StyledContentGrid>
   );
 }
+
+const StyledContentGrid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 1rem;
+  padding-inline: 0;
+`;
