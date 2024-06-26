50d71d1ed42e81fde457df7fe126ea7745562f6e
mlnwns <mlnwns@naver.com>
2024-06-26 12:15:34 +0900

    feat: 공개키 숨김

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wnsbljczzfhovajxhsaw.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Induc2JsamN6emZob3Zhanhoc2F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkwNzQwMjcsImV4cCI6MjAzNDY1MDAyN30.0UncKYWwPwczXWVChu3-RC_xkv1YEco5jV4ZVLzjTts";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
